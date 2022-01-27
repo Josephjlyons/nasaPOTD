00814da7cf3968b5b01bfedfdc6d5eeeb5cea3fb
josephjlyons <josephjlyons90@gmail.com>
2022-01-27 14:54:50 -0500

    build out paths with browserRouter

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import NasaPhoto from './components/NasaPhoto';
+import './App.css';
+
+function App() {
+  return (
+    <BrowserRouter>
+
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path='/nasaphoto' element={<NasaPhoto />} />
+      </Routes>
+
+    </BrowserRouter>
+  );
+}
+
+export default App;
