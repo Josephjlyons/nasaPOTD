6045e6effffb57ad3f12cb74bd7790a553901da0
josephjlyons <josephjlyons90@gmail.com>
2022-02-09 15:41:11 -0500

    misc/ changes to update app file

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './components/Home';
 import NasaPhoto from './components/NasaPhoto';
 import './App.css';
 import NasaPhotos from './components/NasaPhotos';
+import SearchPhotos from './components/SearchPhotos';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route index element={<Home />} />
         <Route path='/nasaphoto' element={<NasaPhoto />} />
         <Route path='/nasaphotos' element={<NasaPhotos />} />
+        <Route path='/searchphotos' element={<SearchPhotos />} />
       </Routes>
     </div>
     </BrowserRouter>
