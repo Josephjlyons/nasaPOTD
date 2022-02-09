bf9461df75251d1f3e696a1b4c9cef42ac9f5ddd
josephjlyons <josephjlyons90@gmail.com>
2022-02-09 13:45:12 -0500

    clean files move spinner to own comp/css file

---
+++ src/components/LoadSpinner/LoadSpinner.js
@@ -0,0 +1,9 @@
+import './LoadSpinner.css'
+
+const LoadSpinner = () => {
+return( 
+    <div className="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+)
+}
+
+export default LoadSpinner
