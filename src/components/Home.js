229aaf7f88f8494efb31cfd8dceac432e7d29801
josephjlyons <josephjlyons90@gmail.com>
2022-02-17 14:50:58 -0500

    comment out uneed links for now

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,10 +8,10 @@ const Home = (props) => {
         <div className='home'>
             <Link className='link' to='/nasaphoto'>Check Out The Photo Of The Day!</Link>
             <Link className='link' to='/nasaphotos'>See 10 Random Photos from past P.O.T.D.</Link>
-            <Link className='link' to='/searchphotos'>Search by a specific date</Link>
-            <Link className='link' to='/searchform'>Search date Range</Link>
-            <Link className='link' to='/searchformtwo'>Test for passing props</Link>
-            <Link className='link' to='/dateinput'>DateInput</Link>
+            {/* <Link className='link' to='/searchphotos'>Search by a specific date</Link> */}
+            {/* <Link className='link' to='/searchform'>Search date Range</Link> */}
+            {/* <Link className='link' to='/searchformtwo'>Test for passing props</Link> */}
+            {/* <Link className='link' to='/dateinput'>DateInput</Link> */}
         </div>
 
     )
