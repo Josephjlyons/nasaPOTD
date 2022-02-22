1d0bf01531f6e9fce040b4956a3d3f41948fda3e
josephjlyons <josephjlyons90@gmail.com>
2022-02-22 13:14:09 -0500

    comment back in searchform for testing

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,7 +9,7 @@ const Home = (props) => {
             <Link className='link' to='/nasaphoto'>Check Out The Photo Of The Day!</Link>
             <Link className='link' to='/nasaphotos'>See 10 Random Photos from past P.O.T.D.</Link>
             {/* <Link className='link' to='/searchphotos'>Search by a specific date</Link> */}
-            {/* <Link className='link' to='/searchform'>Search date Range</Link> */}
+            <Link className='link' to='/searchform'>Search date Range</Link>
             {/* <Link className='link' to='/searchformtwo'>Test for passing props</Link> */}
             {/* <Link className='link' to='/dateinput'>DateInput</Link> */}
         </div>
