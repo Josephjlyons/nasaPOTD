1e615c86e8ce123d75b330ed521fa81f6bc35dc0
josephjlyons <josephjlyons90@gmail.com>
2023-07-17 10:48:55 -0400

    cleanup unused code

--- src/App.js
+++ src/App.js
@@ -1,63 +1,39 @@
-import React, { useState } from 'react';
-import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
+// import React, { useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import NasaPhoto from './components/NasaPhoto';
-import './App.css';
-import NasaPhotos from './components/NasaPhotos';
-import SearchPhotos from './components/SearchPhotos';
+import './App.scss';
+import './scss/_base.scss';
+import NasaPhotoCollection from './components/NasaPhotoCollection';
+import PhotoSearchRender from './components/PhotoSearchRender';
 import SearchForm from './components/SearchForm';
-// import SearchFormTwo from './components/SearchFormTwo;'
+
+
 
 
 const App = (props) => {
 
-  const [enteredStartDate, setEnteredStartDate] = useState('');
-  const [enteredEndDate, setEnteredEndDate] = useState('');
-  // const [formIsShown, setFormIsShown] = useState(true);
 
-  const startDateInputChangeHandler = event => {
-    setEnteredStartDate(event.target.value)
 
-  };
-  const endDateInputChangeHandler = event => {
-    setEnteredEndDate(event.target.value)
-  }
 
-  const formSubmissionHandler = event => {
-    event.preventDefault();
-  };
+
 
   return (
 
     <BrowserRouter>
-      <div className='app'>
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path='/nasaphoto' element={<NasaPhoto />} />
-          <Route path='/nasaphotos' element={<NasaPhotos />} />
-          <Route path='/searchform' element={<SearchForm  />} />
-          <Route path='/searchphotos' element={<SearchPhotos startDate={enteredStartDate} endDate={enteredEndDate} />} />
-          {/* <Route path='/searchformtwo' element={<SearchFormTwo />} /> */}
-        </Routes>
-      </div>
-      <br />
-      {/* <div>
-        <form onSubmit={formSubmissionHandler}>
-          <div className='control-group'>
-            <h1>Search a date range</h1>
-            <div>
-              <label htmlFor='startDate'> Enter A Start Date: </label>
-              <input className='input' type='text' id='startDate' placeholder='yyyy-mm-dd' onChange={startDateInputChangeHandler} />
-            </div>
-            <div>
-              <label htmlFor='endDate'>Enter An End Date: </label>
-              <input className='input' type='endDate' id='endDate' placeholder='yyyy-mm-dd' onChange={endDateInputChangeHandler} />
-            </div>
-            <Link  to='/searchphotos' className='btn'>Submit</Link>
-          </div>
-        </form>
-      </div> */}
 
+
+      <div className='app'>
+        <div className='container'>
+          <Routes>
+            <Route index element={<Home />} />
+            <Route path='/nasaphoto' element={<NasaPhoto />} />
+            <Route path='/nasaphotos' element={<NasaPhotoCollection />} />
+            <Route path='/searchform' element={<SearchForm />} />
+            <Route path='/photosearchrender' element={<PhotoSearchRender  />} />
+          </Routes>
+        </div>
+      </div>
     </BrowserRouter>
   );
 }
